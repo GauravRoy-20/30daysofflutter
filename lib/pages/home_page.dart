9949d4ef0c22c4381b6060e26f7fae814736bd44
GauravRoy-20 <gauravroy20.gr@gmail.com>
2023-06-06 22:57:03 +0530

    completed day16

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -41,19 +41,54 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Catalog App"),
+        title: const Text(
+          "Catalog App",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) {
-                  return ProductItem(
-                    item: CatalogModel.items[index],
-                  );
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
+                itemBuilder: (count, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.deepPurple),
+                            child: Text(
+                              item.name,
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        footer: Container(
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.black),
+                            child: Text(
+                              '\$${item.price}',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        child: Image.network(item.image),
+                      ));
                 },
+                itemCount: CatalogModel.items.length,
               )
+
+            // ListView.builder(
+            //     itemCount: CatalogModel.items.length,
+            //     itemBuilder: (context, index) {
+            //       return ProductItem(
+            //         item: CatalogModel.items[index],
+            //       );
+            //     },
+            //   )
             : const Center(
                 child: CircularProgressIndicator(),
               ),
